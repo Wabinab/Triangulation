8e86fc898ec108ec6954617d3ccaab28402e2974
Wabinab <chowjunwei37@gmail.com>
2024-02-09 08:43:15 +0000

    checkpoint

---
+++ quic_backend/src/main.rs
@@ -0,0 +1,158 @@
+use std::{
+    fs::{self, File},
+    io::{self, Read, Write},
+    path::{self, Path, PathBuf},
+};
+
+use anyhow::{Context, Result};
+use log::{info, error};
+
+use bytes::Bytes;
+use clap::Parser;
+// use rustls::pki_types::PrivateKeyDer;
+use rustls::{Certificate, PrivateKey};
+use webtransport_quinn::{RecvStream, SendStream, Session};
+
+// https://stackoverflow.com/questions/73429672/how-do-i-import-a-file-from-a-folder-in-main
+// use server_config ::*;
+
+// mod server_config;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "[::]:4443")]
+    addr: std::net::SocketAddr,
+
+    /// Use the certificates at this path, encoded as PEM.
+    #[arg(long, default_value = "../cert/localhost.crt")]
+    pub tls_cert: path::PathBuf,
+
+    /// Use the private key at this path, encoded as PEM.
+    #[arg(long, default_value = "../cert/localhost.key")]
+    pub tls_key: path::PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>  {
+    // Enable info logging.
+    let env = env_logger::Env::default().default_filter_or("info");
+    env_logger::init_from_env(env);
+
+    let args = Args::parse();
+
+    // Read the PEM certificate chain
+    let chain = fs::File::open(args.tls_cert.clone()).context("failed to open cert file")?;
+    let mut chain = io::BufReader::new(chain);
+
+    let chain: Vec<Certificate> = rustls_pemfile::certs(&mut chain)?
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    // let chain = rustls_pemfile::certs(&mut chain)
+    //     .collect::<Result<Vec<_>, _>>()
+    //     .unwrap();
+
+    anyhow::ensure!(!chain.is_empty(), "could not find certificate");
+
+    // Read the PEM private key
+    let keys = fs::File::open(args.tls_key).context("failed to open key file")?;
+    let mut keychain = io::BufReader::new(keys);
+
+    // Read the keys into a Vec so we can parse it twice.
+    // let mut buf = Vec::new();
+    // keys.read_to_end(&mut buf)?;
+
+    // Try to parse a PKCS#8 key
+    // -----BEGIN PRIVATE KEY-----
+    // let mut keys = rustls_pemfile::pkcs8_private_keys(&mut keychain)
+    //     .collect::<Result<Vec<_>, _>>()
+    //     .unwrap();
+    let mut keys: Vec<PrivateKey> = rustls_pemfile::pkcs8_private_keys(&mut keychain)?
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // // We can't assign it back to `keys` because it's of a different type since v0.22. 
+    // let mut keys = rustls_pemfile::ec_private_keys(&mut keychain)
+    //     .collect::<Result<Vec<_>, _>>()
+    //     .unwrap();
+    
+    anyhow::ensure!(!keys.is_empty(), "could not find private key");
+    anyhow::ensure!(keys.len() < 2, "expected a single key");
+
+    let key = keys.remove(0);
+    // let key: PrivateKeyDer<'_> = keys.remove(0).try_into().unwrap();
+
+    // Standard Quinn setup (updated, test if it works)
+    let mut tls_config = rustls::ServerConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_protocol_versions(&[&rustls::version::TLS13])
+        .unwrap()
+        .with_no_client_auth()
+        .with_single_cert(chain, key)?;
+    // let mut tls_config = rustls::ServerConfig::builder_with_protocol_versions(&[&rustls::version::TLS13])
+    //     .with_no_client_auth()
+    //     .with_single_cert(chain, key)?;
+
+    tls_config.max_early_data_size = u32::MAX;
+    tls_config.alpn_protocols = vec![webtransport_quinn::ALPN.to_vec()]; // this one is important
+    
+    let config = quinn::ServerConfig::with_crypto(std::sync::Arc::new(tls_config));
+
+    info!("listening on {}", args.addr);
+
+    let server = quinn::Endpoint::server(config, args.addr)?;
+
+    // Accept new connections.
+    while let Some(conn) = server.accept().await {
+        tokio::spawn(async move {
+            let err = run_conn(conn).await;
+            if let Err(err) = err {
+                error!("connection failed: {}", err)
+            }
+        });
+    }
+
+    Ok(())
+}
+
+async fn run_conn(conn: quinn::Connecting) ->  anyhow::Result<()> {
+    // Waiting for QUIC handshake.
+    let conn = conn.await.context("Failed to accept QUIC connection.")?;
+    // Perform WebTransport Handshake.
+    let request = webtransport_quinn::accept(conn.clone()).await?;
+    info!("Request: {}", request.url());
+
+    let path = request.url().clone().path().to_owned();
+
+    // Accept session. 
+    let session = request.ok().await.context("Failed to accept session")?;
+
+    if let Err(err) = run_session(session, path).await {
+        info!("closing session: {}", err);
+    }
+
+    Ok(())
+}
+
+async fn run_session(session: Session, path: String) -> anyhow::Result<()> {
+    loop {
+        tokio::select! {
+            res = session.accept_bi() => {
+                let (send, recv) = res?;
+
+                handle_path_bi(send, recv, path.clone()).await;
+            },
+            res = session.read_datagram() => {
+                let msg = res?;
+            }
+        }
+    }
+}
+
+
+async fn handle_path_bi(send: SendStream, recv: RecvStream, path: String) {
+    
+}
