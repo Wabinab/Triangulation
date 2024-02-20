a5665bf5696792b0978c95f3197f6f6901700169
Wabinab <chowjunwei37@gmail.com>
2024-02-20 07:41:55 +0000

    checkpoint

--- README.learning.md
+++ README.learning.md
@@ -19,3 +19,24 @@ Inside **angular.json**:
   }
 }
 ```
+
+### Optimization
+#### Build
+Build first: 
+
+```bash
+cd frontend
+ng build --configuration production --ssr
+```
+#### CSS
+Using purgecss we can try **but beware, always test program works.** Assume we already built it (and we're in root directory of project)
+
+```bash
+npm i -g purgecss
+cd frontend/dist/frontend
+purgecss -css browser/*.css --content browser/index.html browser/*.js -o browser/
+```
+
+This will write back to `browser/`. If we don't want that, we can write in a new directory. **Make sure you create the folder manually, as it won't create for you.**
+
+### 
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,23 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HomeComponent } from './components/home/home.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { TemplateComponent } from './components/template/template.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { SettingsComponent } from './components/settings/settings.component';
+// import { TemplateComponent } from './components/template/template.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'Triangulation' },
-    { path: 'settings', component: SettingsComponent, title: 'Settings' },
-    { path: 'template', component: TemplateComponent, title: 'Template' },
-    { path: '**', component: NotFoundComponent, title: 'Page Not Found' }
+    // Not lazily loaded. 
+    // { path: '', component: HomeComponent, title: 'Triangulation' },
+
+    // Lazily loaded
+    { pathMatch: "full", path: "", loadComponent: () => import(
+        "./components/home/home.component"
+    ).then((m) => m.HomeComponent), title: 'Triangulation'},
+    { path: "settings", loadComponent: () => import(
+        "./components/settings/settings.component"
+    ).then((m) => m.SettingsComponent), title: 'Settings' },
+    { path: 'template', loadComponent: () => import(
+        "./components/template/template.component"
+    ).then((m) => m.TemplateComponent), title: 'Template' },
+
+    { path: '**', component: NotFoundComponent, title: 'Page Not Found' },
 ];
